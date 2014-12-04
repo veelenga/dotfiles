6339ae857359302f4329b253f4df42329f9115bb
veelenga <velenhaupt@gmail.com>
2014-12-04 23:42:42 +0200

    Restore dotfiles script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 ############################
-# .make.sh
+# restore.sh
 # This script creates symlinks from the home directory to any desired dotfiles in desired directory
 ############################
 
 ########## Variables
 
 dir=$GIT_REPOS/dotfiles           # dotfiles directory
-files="vimrc gvimrc ctags i3blocks.conf i3/config i3/bin/display.sh config zshrc tmux.conf"
+temp=$GIT_REPOS/dotfiles/temp     # backup directory
+files="vimrc gvimrc ctags i3blocks.conf i3/ zshrc tmux.conf"
 
 ##########
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
 cd $dir
-echo "done"
+echo "done\n"
 
-# move any existing dotfiles to dotfiles directory,
+if [ -e $temp ]; then
+  rm -r $temp/
+fi
+
+mkdir $temp
+
+# move any existing dotfiles to backup directory,
 # then create symlinks from the homedir to any files in the dotfiles directory
 # specified in $files
 for file in $files; do
-    echo "Moving .$file to $dir"
-    mv ~/.$file ./$file
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    if [ -e ~/.$file ]; then
+      mv ~/.$file $temp/$file
+    fi
+    if [ -e $dir/$file ]; then
+      cp -rs $dir/$file ~/.$file
+      ls -al ~/ | grep "\.$file"
+    fi
 done
+
+echo "\nYour old dotfiles were saved in $temp directory if any"
