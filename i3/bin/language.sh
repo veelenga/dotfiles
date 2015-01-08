17013861f20506d2b6357243d41b204fb5ce5124
Vitalii Elenhaupt <velenhaupt@gmail.com>
2015-01-08 22:53:44 +0200

    Language support

--- i3/bin/language.sh
+++ i3/bin/language.sh
@@ -1,3 +1,12 @@
-xdotool key 'ctrl+shift'
-exit 0
+lang=${1:-"us"}
 
+if [ "$lang" == "us" ]
+then
+  layout=us,ua
+else
+  layout=ua,us
+fi
+
+setxkbmap -layout $layout -option -option caps:super
+
+exit 0
