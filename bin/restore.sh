8f39a87f843dc8edafb2631c88e61bbf2a95e33d
Vitalii Elenhaupt <velenhaupt@gmail.com>
2015-01-23 12:29:00 +0200

    Added mocp config

--- bin/restore.sh
+++ bin/restore.sh
@@ -28,7 +28,7 @@ pretty_symlink() {
 
 dir=$GIT_REPOS/dotfiles           # dotfiles directory
 temp=$GIT_REPOS/dotfiles/temp     # backup directory
-files="vimrc gvimrc ctags zshrc i3blocks.conf i3/ notify-osd compton.conf Xdefaults"
+files="vimrc gvimrc ctags zshrc i3blocks.conf i3/ notify-osd compton.conf Xdefaults moc/"
 
 ##########
 
