7f4c48f682221c5a513ca2d55b9d22abdf1e7f1d
Vitalii Elenhaupt <velenhaupt@gmail.com>
2020-05-14 14:06:17 +0300

    emacs: do not auto configure lsp

--- doom.d/config.el
+++ doom.d/config.el
@@ -11,6 +11,7 @@
  gc-cons-threshold 100000000
  read-process-output-max (* 1024 1024)
  lsp-prefer-capf t
+ lsp-auto-configure nil
  lsp-idle-delay 0.500)
 (set-face-attribute 'region nil :background "#fdff00")
 
